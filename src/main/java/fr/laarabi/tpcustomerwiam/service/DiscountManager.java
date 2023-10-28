4f6d66be0e36bafe6e396e08be7a3fcac5f4aa1d
Wiam_Laarabi <wiamlaarabi2001@gmail.com>
2023-10-28 08:48:11 +0100

    Ajout d'un bean CDI pour gérer les données de la base sur les codes de réduction

---
+++ src/main/java/fr/laarabi/tpcustomerwiam/service/DiscountManager.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.laarabi.tpcustomerwiam.service;
+
+import fr.laarabi.tpcustomerwiam.Discount;
+import java.util.List;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+
+@RequestScoped
+public class DiscountManager {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Discount> getAllDiscounts() {
+        Query query = em.createNamedQuery("Discount.findAll");
+        return query.getResultList();
+    }
+
+    public Discount findById(String code) {
+        return em.find(Discount.class, code);
+    }
+
+    @Transactional
+    public void persist(Discount discount) {
+        em.persist(discount);
+    }
+}
